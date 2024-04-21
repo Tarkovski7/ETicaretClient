3e644461c10401c5e7697124df853b9719f2d29c
Tarkovski7 <trkbgr1618@gmail.com>
2024-04-21 18:08:29 +0300

    added Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 
-declare var $: any
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor() {}
 }
-
-
-
